23cde3dd3a7eb895ef42df852a1176ce95fe2d9f
Josiah Sayers <josiah.sayers15@gmail.com>
2020-02-02 18:52:18 -0500

    adds author component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppComponent } from './app.component';
 import { SharedServicesModule } from './shared/services/shared-services.module';
 import { PostComponent } from './post/post.component';
 import { SpinnerComponent } from './shared/components/spinner/spinner.component';
+import { AuthorComponent } from './post/author/author.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostComponent,
-    SpinnerComponent
+    SpinnerComponent,
+    AuthorComponent
   ],
   imports: [
     BrowserModule,
